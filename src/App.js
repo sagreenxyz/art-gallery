020ba72d4db92dc9e2fcba5dfe9d40461b452b04
Scott Green <sagreenxyz@gmail.com>
2022-03-23 16:20:46 -0500

    8. refactored Gallery and ButtonBar from App component into separate files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
 import { useState, useEffect } from 'react';
+import Gallery from './components/Gallery';
+import ButtonBar from './components/ButtonBar'
 
 const MOMAURL = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
-
-function Gallery(props) {
-  return (
-    <div style={{ 'width': '100%' }}>
-      <img src={props.objectImg} alt={props.title} />
-      <p>{props.artist}</p>
-    </div>
-  )
-}
-
-function ButtonBar(props) {// in ButtonBar.js
-  return (
-    <div>
-      <button value={-5} onClick={props.handleIterate}>Way Back</button>
-      <button value={-1} onClick={props.handleIterate}>Back</button>
-      <button value={1} onClick={props.handleIterate}>Next</button>
-      <button value={5} onClick={props.handleIterate}>Big Next</button>
-    </div>
-  )
-}
 
 function App() {
   let [artId, setArtId] = useState(12720)
