69f0204f229fe7d166a82dd330b8789cb5f3793e
Scott Green <sagreenxyz@gmail.com>
2022-03-23 15:11:27 -0500

    3. setting up Gallery and ButtonBar components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+function Gallery() {
+  return (
+    <div>Gallery</div>
+  )
+}
+
+function ButtonBar() {
+  return (
+    <div>Button Bar</div>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-
+      <Gallery />
+      <ButtonBar />
     </div>
   );
 }
